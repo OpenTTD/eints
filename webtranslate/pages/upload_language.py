f4d4f54c4feb35c9c6c6d1239c971a4f44e1b137
Alberth <alberth@openttd.org>
2013-02-10 17:05:36 +0100

    Add: More cases for uploading a translation file.

--- webtranslate/pages/upload_language.py
+++ webtranslate/pages/upload_language.py
@@ -48,13 +48,78 @@ def page_post(proj_name):
     user = None # XXX Get user
     stamp = data.make_stamp()
     # XXX Implement the other cases as well.
-    assert base_language and pdata.base_language is None
+    assert base_language
     assert not is_existing
     assert override
 
+    if ng_data.language_data.isocode not in pdata.languages:
+        # Adding a new language.
+        result = add_new_language(ng_data, pdata, base_language)
+        if not result[0]:
+            abort(404, result[0])
+            return
+        lng = result[1]
+
+    else:
+        # Updating a language
+        pass # XXX Remove old stuff
+
+    if base_language:
+        # Add strings as changes.
+        for sv in ng_data.strings:
+            base_text = data.Text(sv.text, sv.case, stamp)
+            chg = data.Change(sv.name, sv.case, base_text, None, stamp, user)
+            chgs = lng.changes.get(chg.string_name)
+            if chgs is None:
+                lng.changes[chg.string_name] = [chg]
+            else:
+                found = None
+                for c in chgs:
+                    if c.case == chg.case and c.base_text == chg.base_text:
+                        found = c # String already present.
+                        break
+                if found is not None: # Found a duplicate.
+                    if override: # Don't mind other changes at all.
+                        found.stamp = chg.stamp
+                        found.user = chg.user
+                    continue
+
+                chgs.append(chg)
+                continue
+
+        # XXX Incorporate loaded data in other files too.
+
+    else:
+        # Add strings as a translation.
+        pass
+        # XXX
+
+    config.cache.save_pmd(pmd)
+    return template('upload_ok', proj_name = proj_name)
+
+
+def add_new_language(ng_data, pdata, base_lang):
+    """
+    Construct a new language, and add it to the project if it does not exist already.
+    Returns the added language or an error description.
+
+    @param ng_data: Loaded language file.
+    @type  ng_data: L{NewGrfData}
+
+    @param pdata: Project data.
+    @type  pdata: L{Project}
+
+    @param base_lang: Is the new language the base language?
+    @type  base_lang: C{bool}
+
+    @return: The language if it could be added, else C{None}.
+    @rtype:  (C{True},L{Language}) or (C{False}, C{str})
+    """
     if ng_data.language_data.isocode in pdata.languages:
-        abort(404, "Language {} already exists".format(ng_data.language_data.isocode))
-        return
+        return (False, "Language {} already exists".format(ng_data.language_data.isocode))
+
+    if pdata.base_language is None and not base_lang: # We don't have a base language, and this is not one either.
+        return (False, "Project has no base language yet, so translations are not possible")
 
     lng = data.Language(ng_data.language_data.isocode)
     lng.grflangid = ng_data.grflangid
@@ -62,21 +127,12 @@ def page_post(proj_name):
     lng.gender = ng_data.gender
     lng.cases  = ng_data.case
     lng.changes = {}
-
-    assert len(pdata.languages) == 0 # XXX Incorporate loaded data in other files too otherwise.
-    # Copy string changes into the language.
-    for sv in ng_data.strings:
-        base_text = data.Text(sv.text, sv.case, stamp)
-        chg = data.Change(sv.name, sv.case, base_text, None, stamp, user)
-        chgs = lng.changes.get(chg.string_name)
-        if chgs is None:
-            lng.changes[chg.string_name] = [chg]
-        else:
-            chgs.append(chg)
-
     pdata.languages[lng.name] = lng
-    pdata.base_language = lng.name
-    pdata.skeleton = ng_data.skeleton
 
-    config.cache.save_pmd(pmd)
-    return template('upload_ok', proj_name = proj_name)
+    # Make it a base language if needed.
+    if base_language:
+        pdata.base_language = lng.name
+        pdata.skeleton = ng_data.skeleton
+
+    return (True, lng)
+
