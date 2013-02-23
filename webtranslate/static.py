9be799d7ed74ca41e0a5308f759f927c6bc7409e
Andy <andy@teamrubber.com>
2013-02-23 16:25:39 +0000

    Codechange: remove unncessary imports from static.py

--- webtranslate/static.py
+++ webtranslate/static.py
@@ -1,4 +1,4 @@
-from webtranslate.bottle import Bottle, run, route, static_file, view, template, post, request, url
+from webtranslate.bottle import route, static_file, url
 
 @route('/static/:path#.+#', name='static')
 def static(path):
