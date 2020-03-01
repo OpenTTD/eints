4188c358bb9dd163e28d5bde4d2d00ba18e015d7
frosch <frosch@openttd.org>
2020-03-01 17:02:36 +0100

    Codechange: Integrate silly.py into development.py

--- webtranslate/users/development.py
+++ webtranslate/users/development.py
@@ -3,8 +3,58 @@ User administration system to use during development.
 
 In particular the user management is very poor.
 """
+import os
 from webtranslate import rights, userauth
-from webtranslate.users import silly
+
+# Table with user names and plain text passwords.
+_users = None
+
+FILENAME = "users.dat"
+
+def init_users():
+    """
+    Initialize the user authentication system.
+    """
+    global _users
+
+    _users = set()
+    if not os.path.isfile(FILENAME): return
+
+    handle = open(FILENAME, "r", encoding = "utf-8")
+    for line in handle:
+        line = line.rstrip()
+        if len(line) == 0 or line[0] == '#': continue
+        i = line.find(':')
+        if i < 0: continue
+        j = line.find(':', i + 1)
+        if j < 0:
+            j = len(line)
+        uname, pwd = line[:i].strip(), line[i+1:j]
+
+        if len(uname) == 0 or len(pwd) == 0: continue
+        _users.add((uname, pwd))
+
+    handle.close()
+
+def authenticate(user, pwd):
+    """
+    Is the provided user and password combination valid?
+
+    @param user: User name.
+    @type  user: C{str}
+
+    @param pwd: Password.
+    @type  pwd: C{str}
+
+    @return: Whether the combination is valid (C{True} means 'valid').
+    @rtype:  C{bool}
+    """
+    global _users
+
+    if len(pwd) == 0: return False
+    return (user, pwd) in _users
+
+
 
 class DevelopmentUserAuthentication(userauth.UserAuthentication):
     """
@@ -20,7 +70,7 @@ def init():
     """
     Initialize the user admin system.
     """
-    silly.init()
+    init_users()
     rights.init_page_access()
     rights.init_projects()
 
@@ -37,7 +87,7 @@ def get_authentication(user, pwd):
     @return: UserAuthentication object to test accesses with.
     @rtype: C{UserAuthentication}
     """
-    if user is None or pwd is None or not silly.authenticate(user, pwd):
+    if user is None or pwd is None or not authenticate(user, pwd):
         return DevelopmentUserAuthentication(False, "unknown")
     else:
         return DevelopmentUserAuthentication(True, user)
