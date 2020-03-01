743344c6e20713621f6c62f787bd4d47adba8ff0
frosch <frosch@openttd.org>
2020-03-01 17:41:48 +0100

    Add: Cookie-bases sessions, login and logout.

--- webtranslate/protect.py
+++ webtranslate/protect.py
@@ -1,14 +1,46 @@
 """
 Bottle authentication decorator.
 """
-from webtranslate.bottle import request, abort, error, response
+import secrets, datetime
+from urllib.parse import quote
+from webtranslate.bottle import request, response, abort, error, response
 from webtranslate.utils import redirect
-from webtranslate import users, config
+from webtranslate import config, userauth
 
-@error(401)
-def handle401(error):
-    response.set_header('WWW-Authenticate', 'Basic realm="Web translator"')
-    return 'Access denied'
+_sessions = dict()
+SESSION_COOKIE = "eints_sid"
+MAX_SESSION_AGE = datetime.timedelta(hours=16)
+
+def cleanup_sessions():
+    now = datetime.datetime.utcnow()
+    for sid, session in _sessions.items():
+        if now > session.expires:
+            del _sessions[sid]
+
+def start_session(userauth):
+    cleanup_sessions()
+
+    userauth.sid = secrets.token_hex(32)
+    userauth.expires = datetime.datetime.utcnow() + MAX_SESSION_AGE
+    _sessions[userauth.sid] = userauth
+    response.set_cookie(SESSION_COOKIE, userauth.sid, expires=userauth.expires, httponly=True)
+
+def get_session():
+    sid = request.get_cookie(SESSION_COOKIE)
+    if sid is not None:
+        session = _sessions.get(sid)
+        if session is not None:
+            if datetime.datetime.utcnow() < session.expires:
+                return session
+            else:
+                stop_session()
+    return userauth.UserAuthentication(False, "unknown")
+
+def stop_session():
+    sid = request.get_cookie(SESSION_COOKIE)
+    if sid is not None and sid in _sessions:
+        del _sessions[sid]
+    response.delete_cookie(SESSION_COOKIE, httponly=True)
 
 METHODS = {'GET': 'read', 'POST': 'add', 'PUT': 'set', 'DELETE': 'del'}
 
@@ -23,15 +55,10 @@ def protected(page_name):
     """
     def decorator(func):
         def wrapper(*a, **ka):
-            if request.auth is None:
-                user, pwd = None, None
-            else:
-                user, pwd = request.auth
-
             pname = [ka.get(p, p) for p in page_name] + [METHODS.get(request.method, "-")]
             prjname = ka.get('prjname')
             lngname = ka.get('lngname')
-            userauth = users.get_authentication(user, pwd)
+            userauth = get_session()
 
             # We must read all uploaded content before returning a response.
             # Otherwise the connection may be closed by the server and the client aborts.
@@ -40,13 +67,13 @@ def protected(page_name):
 
             if userauth is None:
                 # No authentication backend.
-                abort(401, "Access denied")
+                abort(403, "Access denied")
             elif userauth.may_access(pname, prjname, lngname):
                 # Access granted.
                 return func(userauth, *a, **ka)
             elif not userauth.is_auth:
                 # Not logged in.
-                abort(401, "Access denied")
+                redirect("/login", redirect = request.path)
             elif prjname is not None and prjname in config.cache.projects:
                 # Valid user, but insufficient permissions: Go to project page.
                 redirect("/project/<prjname>", prjname = prjname.lower(), message = 'Access denied')
