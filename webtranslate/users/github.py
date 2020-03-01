deb34a1f5ebb2ea1d99efe541abf2845f85238d2
frosch <frosch@openttd.org>
2020-03-01 18:04:11 +0100

    Add: Github oauth-based authentication and team-based authorization.

---
+++ webtranslate/users/github.py
@@ -0,0 +1,139 @@
+"""
+User authentication and authorization via github teams.
+
+"""
+import configparser, os, json, requests, requests_oauthlib
+from webtranslate import rights, userauth
+
+
+
+github_organization = None
+github_org_api_token = None
+github_oauth_client_id = None
+github_oauth_client_secret = None
+
+
+
+def request_teams(name):
+    """
+    Request all teams an user is member of.
+
+    @param name: Github login
+    @type  name: C{str}
+
+    @return: Teams
+    @rtype:  C{set} of C{str}
+    """
+
+    if not github_organization or not github_org_api_token:
+        return set()
+
+    try:
+        query = """query($org:String!, $user:String!) { organization(login: $org) { teams(first: 100, userLogins: [$user]) { nodes { name } } } }"""
+        variables = { "org": github_organization, "user": name }
+        r = requests.post("https://api.github.com/graphql", headers = { "Authorization": "bearer " + github_org_api_token }, json={'query': query, 'variables': variables})
+        if r.status_code != 200:
+            raise RuntimeError("HTTP request returned status {}".format(r.status_code))
+
+        r = r.json()
+        teams = set( t["name"] for t in r["data"]["organization"]["teams"]["nodes"] )
+        return teams
+    except Exception as ex:
+        print(ex)
+        return set()
+
+
+class GithubUserAuthentication(userauth.UserAuthentication):
+    """
+    Implementation of UserAuthentication for Github authentication system.
+    """
+    def __init__(self):
+        super(GithubUserAuthentication, self).__init__(False, "unknown")
+        self.teams = set()
+        self.userid = None
+        self.state = None
+        self.redirect = None
+
+    def start_oauth(self, req_redirect, req_login):
+        self.redirect = req_redirect
+        oauth = requests_oauthlib.OAuth2Session(github_oauth_client_id)
+        if req_login is not None:
+            url, self.state = oauth.authorization_url('https://github.com/login/oauth/authorize', login = req_login)
+        else:
+            url, self.state = oauth.authorization_url('https://github.com/login/oauth/authorize')
+        return url
+
+    def callback(self, request_url):
+        if self.is_auth or self.state is None:
+            return None
+
+        try:
+            oauth = requests_oauthlib.OAuth2Session(github_oauth_client_id, state = self.state)
+            self.state = None # single use, forget now
+
+            oauth.fetch_token('https://github.com/login/oauth/access_token', client_secret = github_oauth_client_secret, authorization_response = request_url)
+            if oauth.authorized:
+                info = oauth.get('https://api.github.com/user').json()
+
+                self.name = info["login"] # can be changed, and reassigned to someone else
+                self.userid = int(info["id"]) # persistent
+
+                # All necessary data is available, now the user is considered authorized
+                self.is_auth = True
+                self.teams = request_teams(self.name)
+
+                return self.redirect
+
+        except Exception as ex:
+            print(ex)
+
+        return None
+
+    def get_roles(self, prjname, lngname):
+        eints_roles = set()
+        if self.is_auth:
+            eints_roles.add('USER')
+
+            if lngname is not None and lngname in self.teams:
+                eints_roles.add('TRANSLATOR')
+
+        return eints_roles
+
+
+
+def init():
+    """
+    Initialize the user admin system.
+    """
+    rights.init_page_access()
+
+def oauth_redirect(req_redirect = None, req_login = None):
+    """
+    Redirect for authentication.
+
+    @param req_redirect: Return URL
+    @type  req_redirect: C{str} or C{None}
+
+    @param req_login: Prefilled login name
+    @type  req_login: C{str} or C{None}
+
+    @return: Tuple of (Session, Redirect URL)
+    @rtype:  (C{userauth.UserAuthentication}, C{str})
+    """
+    userauth = GithubUserAuthentication()
+    return (userauth, userauth.start_oauth(req_redirect, req_login))
+
+def oauth_callback(userauth, request_url):
+    """
+    Callback for authentication.
+
+    @param userauth: Authentication session.
+    @type  userauth: C{userauth.UserAuthentication}
+
+    @param request_url: Complete URL of callback request.
+    @type  request_url: C{str}
+
+    @return: Redirect URL
+    @rtype:  C{str} or C{None}
+    """
+    return userauth.callback(request_url)
