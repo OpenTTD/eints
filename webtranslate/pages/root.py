1861c9b64ebdbf74a90a3f7b2f04eff752a3c458
Patric Stout <truebrain@openttd.org>
2020-09-22 16:15:17 +0200

    Add: a /healthz URL to detect if eints is online by LoadBalancer

--- webtranslate/pages/root.py
+++ webtranslate/pages/root.py
@@ -1,7 +1,7 @@
 """
 Root page.
 """
-from webtranslate.bottle import route
+from webtranslate.bottle import HTTPResponse, route
 from webtranslate.utils import template
 from webtranslate.protect import protected
 
@@ -10,3 +10,8 @@ from webtranslate.protect import protected
 @protected(["root", "-", "-"])
 def root(userauth):
     return template("root", userauth=userauth)
+
+
+@route("/healthz", method="GET")
+def healthz():
+    return HTTPResponse("200: OK", status=200, headers={"content-type": "text/plain"})
